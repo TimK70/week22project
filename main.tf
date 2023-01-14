f15f34aae4cd0e06e64ca1b4d169d077d51a28db
TimK70 <tkrass1@gmail.com>
2023-01-14 16:03:21 +0000

    removed lines from main.tf bastion module

--- main.tf
+++ main.tf
@@ -20,20 +20,9 @@ module "compute" {
 
 module "week22_bastion" {
   source = "./bastion"
-  public_sg        = module.networking.public_sg
-  public_subnets   = module.networking.public_subnets
-  public_subnet_id = module.compute.public_subnet_id
-  ami_id           = module.compute.ami_id
-  instance_count   = 2
-  instance_type    = "t2.micro"
-  vol_size         = 10
-  key_name         = "two_tierkey"
-  public_key_path  = "/home/ec2-user/.ssh/two_tierkey.pub"
-  user_data_path   = "${path.root}/userdata.tpl"
-  db_endpoint      = module.database.db_endpoint
-  dbuser           = var.dbuser
-  dbpassword       = var.dbpassword
-  dbname           = var.dbname
+  public_cidrs   = ["10.0.2.0/24", "10.0.4.0/24", "10.0.6.0/24"]
+  public_sn_count = var.public_sn_count
+  private_sn_count = var.private_sn_count 
 }
 
 
