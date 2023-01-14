a99006cc87556314d46fd3adda7010392e597e1f
TimK70 <tkrass1@gmail.com>
2023-01-14 21:34:35 +0000

    Modified variables.tf... again

--- variables.tf
+++ variables.tf
@@ -1,28 +1,10 @@
 #---root/variables.tf
-variable "public_sn_count" {
-         value = number
-}
-variable "private_sn_count" {
-         value = list(any)
-}
-variable "public_cidrs" {
-         value = list(any)
-}
-variable "dbuser" {
-         value = string
-}
-variable "dbpassword" {
-         value = string
-}
-variable "dbname" {
-         value = string
-}
-variable "ami_id" {
-         value = string
-}
-variable "public_subnet_id" {
-         value = string
-}
-variable "key_name" {
-         value = string
-}
+variable "public_sn_count" {}
+variable "private_sn_count" {}
+variable "public_cidrs" {}
+variable "dbuser" {}
+variable "dbpassword" {}
+variable "dbname" {}
+variable "ami_id" {}
+variable "public_subnet_id" {}
+variable "key_name" {}
