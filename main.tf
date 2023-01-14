c20dd80488c6755af63c186a176f091df69161f5
TimK70 <tkrass1@gmail.com>
2023-01-14 14:30:47 +0000

    Modified Bastion info in main.tf

--- main.tf
+++ main.tf
@@ -20,6 +20,20 @@ module "compute" {
 
 module "week22_bastion" {
   source = "./compute"
+  public_sg        = module.networking.public_sg
+  public_subnets   = module.networking.public_subnets
+  public_subnet_id = module.compute.public_subnet_id
+  ami_id           = module.compute.ami_id
+  instance_count   = 2
+  instance_type    = "t2.micro"
+  vol_size         = 10
+  key_name         = "two_tierkey"
+  public_key_path  = "/home/ec2-user/.ssh/two_tierkey.pub"
+  user_data_path   = "${path.root}/userdata.tpl"
+  db_endpoint      = module.database.db_endpoint
+  dbuser           = var.dbuser
+  dbpassword       = var.dbpassword
+  dbname           = var.dbname
 }
 
 
