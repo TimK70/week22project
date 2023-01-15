325fbdbc67e88d0f2dace40f6f34a5c37b5297b0
TimK70 <tkrass1@gmail.com>
2023-01-15 14:12:06 +0000

    Uncommented variables

--- main.tf
+++ main.tf
@@ -5,14 +5,14 @@ module "compute" {
   public_sg        = module.networking.public_sg
   public_subnets   = module.networking.public_subnets
   public_subnet_id = var.public_subnet_id
-  ami_id           = module.compute.ami_id
+  ami_id           = module.compute.server_ami
   instance_count   = 2
   instance_type    = "t2.micro"
   vol_size         = 10
   key_name         = "two_tierkey"
   public_key_path  = "/home/ec2-user/.ssh/two_tierkey.pub"
   user_data_path   = "${path.root}/userdata.tpl"
-  db_endpoint      = module.database.db_endpoint
+  # db_endpoint      = module.database.db_endpoint
   dbuser           = var.dbuser
   dbpassword       = var.dbpassword
   db_name           = var.db_name
--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 #---root/variables.tf
-# variable "public_sn_count" {}
-# variable "private_sn_count" {}
-# variable "public_cidrs" {}
-# variable "dbuser" {}
-# variable "dbpassword" {}
-# variable "dbname" {}
-# variable "ami_id" {}
-# variable "public_subnet_id" {}
-# variable "key_name" {}
+variable "public_sn_count" {}
+variable "private_sn_count" {}
+variable "public_cidrs" {}
+variable "dbuser" {}
+variable "dbpassword" {}
+variable "dbname" {}
+variable "ami_id" {}
+variable "public_subnet_id" {}
+variable "key_name" {}
