57c872bfa4a67c09bd448360045204ee03d632ac
TimK70 <tkrass1@gmail.com>
2023-01-15 15:17:43 +0000

    modified .tfvars and variables.tf

--- terraform.tfvars
+++ terraform.tfvars
@@ -5,6 +5,6 @@ private_sn_count = 4
 public_cidrs = ["10.0.2.0/24, 10.0.4.0/24, 10.0.6.0/24"]
 #dbuser
 #dbpassword
-#db_name = 
+db_name = var.dbuser
 #ami_id = "
 #public_subnet_id = 
--- variables.tf
+++ variables.tf
@@ -2,11 +2,9 @@
 variable "public_sn_count" {}
 variable "private_sn_count" {}
 variable "public_cidrs" {}
-variable "dbuser" {
-    value = string
-}
+variable "dbuser" {}
 variable "dbpassword" {}
 variable "dbname" {}
 variable "ami_id" {}
 variable "public_subnet_id" {}
-# variable "key_name" {}
+variable "key_name" {}
